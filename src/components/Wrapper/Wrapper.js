33ca02e381464dec7756276b569093e7d3f83f0f
Claire Stolp <clairemstolp@gmail.com>
2018-09-22 10:59:34 -0400

    configured materialize-UI

---
+++ src/components/Wrapper/Wrapper.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+class Wrapper extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button variant="contained" color="primary">
+          I work
+        </Button>
+      </div>
+    )
+  }
+}
+
+export default Wrapper;
