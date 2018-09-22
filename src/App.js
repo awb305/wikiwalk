577c18094b2fdf24f1a3eab696a8f8c77d2566d0
Claire Stolp <clairemstolp@gmail.com>
2018-09-22 13:54:39 -0400

    set up material themeing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Wrapper from './components/Wrapper';
+import Theme from './Theme';
+
 
 class App extends Component {
+
   render() {
     return (
-      <React.Fragment>
-        <CssBaseline />
-        <Wrapper />
-      </React.Fragment>
+      <Theme>
+        <React.Fragment>
+          <CssBaseline />
+          <Wrapper />
+        </React.Fragment>
+      </Theme>
     );
   }
 }
