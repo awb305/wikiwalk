912d001fae73a13071861a8fde9649f7eb0e0cab
Andrew B <awb305@gmail.com>
2018-09-23 11:08:23 -0400

    progress

--- src/components/sample.jsx
+++ src/components/sample.jsx
@@ -2,15 +2,46 @@ import React, { Component } from 'react';
 import API from '../utils/API';
 
 class Sample extends Component {
+  state = {
+    geoArray: [],
+    idArray: [],
+    content: {}
+  };
+
+  componentDidMount() {
+    this.search();
+  }
+
+  pageIdArray = () => {
+    const idArray = [];
+    this.geoArray.forEach(element => {
+      idArray.push(element.pageid);
+    });
+    this.setState({
+      idArray: idArray
+    });
+  };
+
   search = () => {
-    API.search()
+    API.geoSearch()
+      .then(res => {
+        const geoArray = res.data.query.geosearch;
+        const idArray = [];
+        geoArray.forEach(element => {
+          idArray.push(element.pageid);
+        });
+        this.setState({
+          geoArray: geoArray,
+          idArray: idArray
+        });
+      })
+      .catch(err => console.log(err));
+    API.idSearch(this.state.idArray)
       .then(res => console.log(res))
       .catch(err => console.log(err));
   };
 
   render() {
-    this.search();
-
     return <div>Check the console</div>;
   }
 }
--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,16 @@ import axios from "axios";
 
 //make sure you don't use fat arrow funcitons
 export default {
-  search: function () {
+  geoSearch: function () {
     return axios.get("https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=35.779600%7C-78.638200&gsradius=10000&gslimit=10");
+  },
+  idSearch: function (array) {
+    console.log(array);
+    let url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&prop=info|extracts&inprop=url&redirects=1&pageids="
+    array.forEach(element => {
+      url += "|" + element;
+    });
+    console.log(url);
+    return axios.get(url);
   }
 };
