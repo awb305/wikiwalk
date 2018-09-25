41d3f57339d7642d29ce6a268cff1b30b2d2d9cc
Andrew B <awb305@gmail.com>
2018-09-24 21:54:09 -0400

    changed | to %7C

--- src/components/sample.jsx
+++ src/components/sample.jsx
@@ -6,8 +6,8 @@ class Sample extends Component {
     geoArray: [],
     idArray: [],
     content: {},
-    lon: 35.7796,
-    lat: -78.6382,
+    lon: 43.7696,
+    lat: 11.2558,
     radius: 1000,
     limit: 10
   };
--- src/utils/API.js
+++ src/utils/API.js
@@ -6,8 +6,8 @@ export default {
 
   geoSearch: function (lon, lat, radius, limit) {
     console.log(lon, lat);
-    let url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=" + lon + "|" + lat + "&gsradius=" + radius + "&gslimit=" + limit
-
+    let url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=" + lon + "%7C" + lat + "&gsradius=" + radius + "&gslimit=" + limit
+    console.log(url);
     return new Promise(function (resolve, reject) {
       return axios.get(url)
         .then(function (res) {
@@ -23,7 +23,7 @@ export default {
     console.log(array);
     let url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&prop=info|extracts&inprop=url&redirects=1&pageids="
     array.forEach(element => {
-      url += "|" + element;
+      url += "%7C" + element;
     });
     console.log(url);
     return axios.get(url);
@@ -33,4 +33,6 @@ export default {
 
 //35.779600 - 78.638200
 
+//https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=41.256500%7C-95.934500&gsradius=1000&gslimit=10
 //https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=35.779600%7C-78.638200&gsradius=10000&gslimit=10
+//https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=41.2565|95.9345&gsradius=1000&gslimit=10
